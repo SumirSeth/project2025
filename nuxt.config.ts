53c5fa3bc0abce1aed5b003fb0832842b63884b1
Sumir Seth <iamsumirseth@gmail.com>
2025-01-11 20:38:16 +0530

    update Google API key reference to use NUXT_PUBLIC environment variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   ],
   runtimeConfig: {
     public: {
-      googleApiKey: process.env.GOOGLE_API_KEY,
+      googleApiKey: process.env.NUXT_PUBLIC_GOOGLE_API_KEY,
       motion: {
         directives: {
           'pop-bottom' : {
